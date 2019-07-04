fde3a82e36847124dcb242e4f491be7e275b13b7
sajjad233 <52513967+sajjad233@users.noreply.github.com>
2019-07-04 03:20:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '#'
 
 
-const developers = ["ID","ID"]
+const developers = ["452362309038243841"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
