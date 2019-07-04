8de70a41a0070b76f444cde7b597cd22a976b866
sajjad233 <52513967+sajjad233@users.noreply.github.com>
2019-07-04 03:15:29 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "c";
-var adminprefix = 'Y'
+var prefix = "+";
+var adminprefix = '#'
 
-const developers = ["472413769700474901"]
+
+const developers = ["ID","ID"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
